5bdec3c6c58869ee87906b289d4a6600343423f9
David Housman <david.nyttsheet@gmail.com>
2013-08-26 17:37:12 -0500

    add long and lat code and now log data

--- rainmakerProject/Rainmaker/src/main/java/com/davidhousman/rainmaker/MainActivity.java
+++ rainmakerProject/Rainmaker/src/main/java/com/davidhousman/rainmaker/MainActivity.java
@@ -1,15 +1,65 @@
 package com.davidhousman.rainmaker;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+
+
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+                double myLatitude = location.getLatitude();
+                double myLongitude = location.getLongitude();
+
+                String myLatString = String.valueOf(myLatitude);
+                String myLongString = String.valueOf(myLongitude);
+
+                Log.i("Latitude", myLatString);
+                Log.i("Longitude", myLongString);
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+
+
+
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+
     }
 
 
